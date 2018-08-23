81bb8351b95adc72a681f526ce45435aa4388997
Lzu Tao <1stpangu@gmail.com>
2018-08-23 23:28:39 +0700

    answer/ch02/Exercises: Edit indentation of {01,02,03}.c

--- answer/ch02/Exercises/02.c
+++ answer/ch02/Exercises/02.c
@@ -2,8 +2,8 @@
 
 int main (void)
 {
-	printf("Parkinson's Law:\nWork expands so as to ");
-	printf("fill the time\n");
-	printf("available for its completion.\n");
-	return 0;
+  printf("Parkinson's Law:\nWork expands so as to ");
+  printf("fill the time\n");
+  printf("available for its completion.\n");
+  return 0;
 }
