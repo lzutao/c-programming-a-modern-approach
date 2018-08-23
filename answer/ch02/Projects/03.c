0f6b5c2da8e2d032bb0f943873059a9bc9348ec3
Lzu Tao <1stpangu@gmail.com>
2018-08-23 23:34:53 +0700

    03.c: Handle invalid input value of radius
    I don't know why but the @fordea like to use global variable.
    I changed it to local variable with c99 style, divide it into
    functions and use command line arguments instead of stdin.

--- answer/ch02/Projects/03.c
+++ answer/ch02/Projects/03.c
@@ -1,14 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <errno.h>
 
 #define PI 3.14159
-#define FRAC 4.0f/3.0f
+#define SPHERE_SCALE (4.0 / 3.0)
 
-float radius, volume;
+void fatal(const char *msg);
+double intput_double(const char *str);
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    printf("Please enter the radius of the sphere: ");
-    scanf("%f", &radius); 
-    volume = FRAC * (PI * (radius * radius * radius));
-    printf("%.2f", volume);
+  if (argc < 2) {
+    printf("usage: %s <radius of the sphere>\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  double radius = intput_double(argv[1]);
+
+  if (radius < 0) {
+    fatal("Radius is less than zero.");
+  }
+
+  double volume = SPHERE_SCALE * PI * (radius * radius * radius);
+
+  printf("Volume of the sphere with radius %lf: %lf\n", radius, volume);
+  exit(EXIT_SUCCESS);
+}
+
+void fatal(const char *msg)
+{
+  fprintf(stderr, "error: %s\n", msg);
+  exit(EXIT_FAILURE);
+}
+
+double intput_double(const char *str)
+{
+  char *endptr;
+
+  errno = 0;  /* To distinguish success/failure after call */
+  double value = strtod(str, &endptr);
+
+  /* Check for various possible errors */
+
+  if ((errno == ERANGE && value == HUGE_VAL)
+      || (errno != 0 && value == 0)) {
+    perror("strtod");
+    exit(EXIT_FAILURE);
+  }
+
+  if (endptr == str) {
+    fatal("No digits were found.");
+  }
+
+  /* If we got here, strtod function successfully parsed a number */
+
+  if (isinf(value) || isnan(value)) {
+    fatal("Invalid input number.");
+  }
+
+  return value;
 }
