efe58e62c57cdfb03abfe6c25cbde3b856fe2324
Lzu Tao <1stpangu@gmail.com>
2018-08-25 14:34:41 +0700

    Fix indentation

--- answer/ch03/Exercises/02.c
+++ answer/ch03/Exercises/02.c
@@ -2,12 +2,12 @@
 
 int main(void)
 {
-    float x = 123.456;
+  float x = 123.456;
 
-    printf("%-8.1e\n", x);
-    printf("%10.6e\n", x);
-    printf("%-8.3f\n", x);
-    printf("%6.0f\n", x);
+  printf("%-8.1e\n", x);
+  printf("%10.6e\n", x);
+  printf("%-8.3f\n", x);
+  printf("%6.0f\n", x);
 
-    return 0;
+  return 0;
 }
