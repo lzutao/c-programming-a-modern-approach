8d00d50b4f6722418525dcaf8443498403f4ab96
Lzu Tao <1stpangu@gmail.com>
2018-08-30 10:32:19 +0700

    Remove redundant boolean expression in `else if`

--- answer/ch05/Projects/01.c
+++ answer/ch05/Projects/01.c
@@ -7,15 +7,17 @@ int main(void)
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    if ((num >= -9 && num <= 9))  
+    int absolute_num = (num < 0) ? -num: num;
+
+    if (absolute_num <= 9)
         printf("1 digit");
-    else if ((num >= 10 && num <= 99) || (num <= -10 && num >= -99))
+    else if (absolute_num <= 99)
         printf("2 digits");
-    else if ((num >= 100 && num <= 999) || (num <= -100 && num >= -999))
+    else if (absolute_num <= 999)
         printf("3 digits");
-    else if ((num >= 1000 && num <= 9999) || (num <= -1000 && num >= -9999))
+    else if (absolute_num <= 9999)
         printf("4 digits");
-    else if ((num >= 10000 && num <= 32767) || (num <= -10000 && num >= -32767))
+    else if (absolute_num <= 32767)
         printf("5 digits");
     else
         printf("Integer overflow on 16 bit int");
